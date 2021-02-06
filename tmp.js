4edd869504294eaf426bacea54de851abbb3eac3
will vu <will.vu@raccoons.cc>
2021-02-06 12:29:55 -0500

    css keyframe animations replace

--- tmp.js
+++ tmp.js
@@ -1,16 +1,16 @@
-var loader = document.getElementById('PageLoader');
-var angle = 720;
-var raccoonEmoji = document.getElementById('BigRaccoon');
-raccoonEmoji.style.opacity = '1';
+// var loader = document.getElementById('PageLoader');
+// var angle = 720;
+// var raccoonEmoji = document.getElementById('BigRaccoon');
+// raccoonEmoji.style.opacity = '1';
 
-/* Page Loader */
+// /* Page Loader */
 
-window.addEventListener('load', () => {
-    var spin = setInterval(() => {
-        raccoonEmoji.style.transform = 'rotate(' + angle + 'deg)';
-        angle = angle + 360;
-    }, 1000);
-    setTimeout(() => {
-        loader.style.opacity = 1;
-    }, 800);
-});
+// window.addEventListener('load', () => {
+//     var spin = setInterval(() => {
+//         raccoonEmoji.style.transform = 'rotate(' + angle + 'deg)';
+//         angle = angle + 360;
+//     }, 1000);
+//     setTimeout(() => {
+//         loader.style.opacity = 1;
+//     }, 800);
+// });
